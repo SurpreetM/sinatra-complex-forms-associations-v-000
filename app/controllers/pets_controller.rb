73fa39c222ab5c8106fbb14b60c4ace91f100041
surpreet.mundra <surpreet.mundra@gmail.com>
2019-07-01 15:15:59 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -29,7 +29,7 @@ class PetsController < ApplicationController
     erb :'/pets/edit'
   end
 
-  post '/pets/:id/edit' do
+  post '/pets/:id' do
     @pet = Pet.find(params[:id])
     @pet.update(params["pet"])
     if !params["owner"]["name"].empty?
